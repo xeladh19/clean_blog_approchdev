e423027944c00906c088848f6553c3d8fa5677be
Alexandre <a.dheur.webdesign@gmail.com>
2021-09-07 09:29:54 +0200

    connexion DB + fichiers internes
    Mise en place des fichiers internes dans core,app,public.

---
+++ app/config/params.php
@@ -0,0 +1,14 @@
+<?php 
+/*
+../app/config/params.php
+*/
+//Paramètres de connexion
+define('DB_HOST',       'localhost:3306');
+define('DB_NAME',       'cleanBlog_2017');
+define('DB_USER',       'root');
+define('DB_PASSWORD',   'root');
+
+
+//Initialisation des zones dynamiques
+
+$content = '';
